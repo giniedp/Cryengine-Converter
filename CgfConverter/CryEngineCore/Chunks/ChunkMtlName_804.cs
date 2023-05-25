9396ca754f3383929ddb7b3bac0c31e3a577fd7f
Ginie <github@ginie.eu>
2023-05-25 22:51:45 +0200

    Added ChunkMtlName for version 804

---
+++ CgfConverter/CryEngineCore/Chunks/ChunkMtlName_804.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace CgfConverter.CryEngineCore;
+
+internal sealed class ChunkMtlName_804 : ChunkMtlName
+{
+    public override void Read(BinaryReader b)
+    {
+        base.Read(b);
+        Name = b.ReadFString(128);
+        NumChildren = 0;
+    }
+}
